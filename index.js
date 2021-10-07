7235e801a7464d33e1bee442af125abceac29ae7
jose-mercado <josefmercado99@gmail.com>
2021-10-06 18:25:59 -0700

    monolith

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ const express = require("express")
 const cors = require("cors")
 const app = express()
 const port = process.env.PORT || 9000
+const path = require("path")
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, "client/build")))
 
 app.use("/api/*", (_,res) => {
     res.json({
         data:"This is the API data"
     })
+})
+
+app.use("*",(_,res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"))
 })
 
 app.listen(port, () => {
